92ee6f68aa32ceba9ed18bcd8188d3d4d8d2ecd3
Devin Mitchell <devin@mtechnologies.net>
2024-07-29 23:47:46 -0500

    bruh

--- payments/payment_gateways/stripe_integration.py
+++ payments/payment_gateways/stripe_integration.py
@@ -37,7 +37,7 @@ def create_stripe_subscription(gateway_controller, data):
 def create_subscription_on_stripe(stripe_settings):
 	items = []
 	for subscription_plan in stripe_settings.subscription_plans:
-		priceid = frappe.db.get_value('Subscription Plan', plan.plan, 'product_price_id')
+		priceid = frappe.db.get_value('Subscription Plan', subscription_plan.plan, 'product_price_id')
 		items.append({"price": priceid, "quantity": subscription_plan.qty})
 
 	try:
